052a98adaf7da228aec938bf5fa1ce2f7e859866
nanadjei2 <elvisnti@qodehub.com>
2018-08-02 10:39:08 +0000

    Test middleware - 'auth' when users are not authenticated

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,6 +10,20 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class ParticipateInForumTest extends TestCase
 {
 	use DatabaseMigrations;
+	 /**
+     * A user can participate in a forum
+     * @test
+     * @return void
+     */
+    public function unauthenticated_users_may_not_add_replies() {
+    	$this->expectException('Illuminate\Auth\AuthenticationException');
+    	$thread = factory('App\Thread')->create();
+    	// Given we have a reply.
+    	$reply = factory('App\Reply')->create();
+    	// The user submits a reply.
+    	// $this->post('threads/'.$thread->id.'/replies', $reply->toArray()); also works
+    	$this->post($thread->path().'/replies', $reply->toArray());
+    }
     /**
      * A user can participate in a forum
      * @test
@@ -25,7 +39,8 @@ class ParticipateInForumTest extends TestCase
     	// Given we have a reply.
     	$reply = factory('App\Reply')->create();
     	// The user submits a reply.
-    	$this->post('threads/'.$thread->id.'/replies', $reply->toArray());
+    	// $this->post('threads/'.$thread->id.'/replies', $reply->toArray()); also works
+    	$this->post($thread->path().'/replies', $reply->toArray());
     	$this->get($thread->path())->assertSee($reply->body);
     }
 }
