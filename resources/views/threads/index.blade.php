a7a10180e267f281335926207171226f69c8feef
Adjei Nti Elvis <nana.elvee@gmail.com>
2018-08-06 11:12:27 +0000

    A little work on the thread index

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -2,20 +2,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-10 px-0">
             <div class="card">
                 <div class="card-header">Forum Threads</div>
 
                 <div class="card-body">
                     @foreach($threads as $thread)
                        <article>
-                        <div class="flex">
-                          <a href="{{ $thread->path() }}">
-                             <h4>{{ $thread->title }}</h4>
-                          </a>
+                        <div class="row">
+                          <div class="col-10">
+                            <a href="{{ $thread->path() }}">
+                              <h4>{{ $thread->title }}</h4>
+                            </a>
+                          </div>
+                            <div class="col-2">
+                              <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+                            </div>
                          </div>
-                             <div class="body">{{ $thread->body }}</div>
-                             <strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong>
+                             <div class="body">{{ $thread->body }}</div>      
                        </article>
                        <hr>
                    @endforeach
